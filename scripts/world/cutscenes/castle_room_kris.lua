19ec9d2d0a7cc36ad99ee837d20d87c203d1a29b
Liu Wenyuan <15816141883@163.com>
2022-09-04 12:35:34 +0800

    castle_room_kris moss cleanup

--- scripts/world/cutscenes/castle_room_kris.lua
+++ scripts/world/cutscenes/castle_room_kris.lua
@@ -3,10 +3,9 @@ return {
         cutscene:text("* It's a stand for your Manual.")
         cutscene:text("* I thought you might want to keep it here in case you want to read it!", "blush_smile", "ralsei")
     end,
-    -- * It's some decorative moss. Looks delicious.
     moss = function(cutscene, me, chara, facing)
         cutscene:text("* It's some decorative moss. Looks delicious.")
-        if Game:getPartyMember("susie") then
+        if cutscene:getCharacter("susie") then
             cutscene:text("* Why the hell does YOUR room get moss!?", "nervous_side", "susie")
         end
     end,
